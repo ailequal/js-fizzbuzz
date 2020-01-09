730c1ce7707c163d27465b8b64fe553464bcb25b
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-09 15:46:45 +0100

    fizzbuzz only is working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 // print numbers from 1 to 100
 var arrayNumbers = [];
-for (var i = 1; i <= 100; i++) {
-  arrayNumbers[i - 1] = i;
+for (var i = 0; i < 100; i++) {
+  arrayNumbers.push(i + 1);
+  if (arrayNumbers[i] % 3 !== 0 || arrayNumbers[i] % 5 !== 0) {
+    console.log(arrayNumbers[i]);
+  } else if (arrayNumbers[i] % 3 == 0 && arrayNumbers[i] % 5 == 0) {
+    console.log('fizzbuzz');
+  } else if (arrayNumbers[i] % 3 == 0) {
+    console.log('fizz');
+  } else if (arrayNumbers[i] % 5 == 0) {
+    console.log('buzz');
+  }
 }
+
 console.log(arrayNumbers);
 
 // x3 are 'fizz', not numbers
