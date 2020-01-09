a513a7c62e87494ec771b9be528f45c42d590093
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-09 15:17:20 +0100

    Added basic stuff

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// print numbers from 1 to 100
+var arrayNumbers = [];
+for (var i = 1; i <= 100; i++) {
+  arrayNumbers[i - 1] = i;
+}
+console.log(arrayNumbers);
+
+// x3 are 'fizz', not numbers
+
+// x5 are 'buzz', not numbers
+
+// x3 && xx5 are 'fizzbuzz', not numbers
