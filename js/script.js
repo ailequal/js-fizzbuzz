13bec5ad878b56681ec58db95bea4edc04f6d3ed
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-09 16:20:59 +0100

    Fixed condition

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var arrayNumbers = [];
 for (var i = 0; i < 100; i++) {
   arrayNumbers.push(i + 1);
-  if (arrayNumbers[i] % 3 !== 0 || arrayNumbers[i] % 5 !== 0) {
+  if (arrayNumbers[i] % 3 !== 0 && arrayNumbers[i] % 5 !== 0) {
     console.log(arrayNumbers[i]);
   } else if (arrayNumbers[i] % 3 == 0 && arrayNumbers[i] % 5 == 0) {
     console.log('fizzbuzz');
@@ -12,11 +12,8 @@ for (var i = 0; i < 100; i++) {
     console.log('buzz');
   }
 }
-
 console.log(arrayNumbers);
 
 // x3 are 'fizz', not numbers
-
 // x5 are 'buzz', not numbers
-
-// x3 && xx5 are 'fizzbuzz', not numbers
+// x3 && x5 are 'fizzbuzz', not numbers
